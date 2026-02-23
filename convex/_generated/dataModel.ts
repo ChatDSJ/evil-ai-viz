5cb23f199c3da009df84a16ea3289f3879868feb
Viktor AI <viktor@getviktor.com>
2026-02-23 03:29:37 +0000

    Deploy to GitHub Pages - aiheadquarters.ai

---
+++ convex/_generated/dataModel.ts
@@ -0,0 +1,4 @@
+/* eslint-disable */
+import type { DataModelFromSchemaDefinition } from "convex/server";
+import type schema from "../schema";
+export type DataModel = DataModelFromSchemaDefinition<typeof schema>;
